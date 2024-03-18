bf570008ac5eb1e95184c21fc23bf4d5814e1aac
Emil Holmgaard <124199961+holmek@users.noreply.github.com>
2024-03-18 15:56:59 +0100

    Update main.py

--- iot project/RaspHub-Gateway/main.py
+++ iot project/RaspHub-Gateway/main.py
@@ -1,21 +1,20 @@
 import digitalio
 import board
 import busio
-import adafruit_rfm9x as LoRa
-import paho.mqtt.publish as publish
+import adafruit_rfm9x
+from time import sleep
 
 RADIO_FREQ_MHZ = 433.0
 CS = digitalio.DigitalInOut(board.CE1)
 RESET = digitalio.DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-LoRa = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
-LoRa = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=1000000)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+# Initialze RFM radio with a more conservative baudrate
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=1000000)
 
 while True:
-    received_data = LoRa.receive()
-    if received_data is not None:
-        # Så konvertere den dataen = streng 
+    received_data = rfm9x.receive()
+     if received_data is not None:
         received_string = str(received_data, "utf-8")
         print(received_string)
         publish.single("sg5-2a", received_string, hostname="mqtt.eclipseprojects.io")
-
