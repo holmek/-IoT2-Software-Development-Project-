d2ae57301eccd8c983ba31c25c00a9cdf9e21f78
Emil Holmgaard <124199961+holmek@users.noreply.github.com>
2024-03-18 15:49:24 +0100

    Update main.py

--- iot project/RaspHub-Gateway/main.py
+++ iot project/RaspHub-Gateway/main.py
@@ -2,23 +2,20 @@ import digitalio
 import board
 import busio
 import adafruit_rfm9x as LoRa
+import paho.mqtt.publish as publish
 
-# LoRa - https://github.com/KevinLindemark/lora_esp32_rfm9x
 RADIO_FREQ_MHZ = 433.0
 CS = digitalio.DigitalInOut(board.CE1)
 RESET = digitalio.DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
-# Lora Radio
 LoRa = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
 LoRa = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=1000000)
 
 while True:
-    # Modtag data fra LoRa og så bliver den gemt i received_data
     received_data = LoRa.receive()
-    
-    # Hvis der er modtaget data og det ikke er ingenting
     if received_data is not None:
         # Så konvertere den dataen = streng 
         received_string = str(received_data, "utf-8")
         print(received_string)
+        publish.single("sg5-2a", received_string, hostname="mqtt.eclipseprojects.io")
+
