d83d971a7af92d09f56f761ca0b1b8aa076fddfb
Emil Holmgaard <124199961+holmek@users.noreply.github.com>
2024-03-19 00:15:17 +0100

    Update main.py

--- iot project/RaspHub-Gateway/main.py
+++ iot project/RaspHub-Gateway/main.py
@@ -1,3 +1,4 @@
+  GNU nano 5.4                                                                                                                 mqtt_pub.py                                                                                                                           
 import digitalio
 import board
 import busio
@@ -5,17 +6,15 @@ import adafruit_rfm9x
 from time import sleep
 import paho.mqtt.publish as publish
 
-RADIO_FREQ_MHZ = 433.0
+RADIO_FREQ_MHZ = 868.0
 CS = digitalio.DigitalInOut(board.CE1)
 RESET = digitalio.DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
-# Initialize RFM radio with a more conservative baudrate
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=1000000)
 
 while True:
     received_data = rfm9x.receive()
     if received_data is not None:
         received_string = str(received_data, "utf-8")
-        print(received_string)
         publish.single("sg5-2a", received_string, hostname="mqtt.eclipseprojects.io")
