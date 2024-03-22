d2d21c28a89f4875764a73d4c798a69f12934801
Emil Holmgaard <124199961+holmek@users.noreply.github.com>
2024-03-22 13:34:00 +0100

    Update main.py

--- iot project/RaspHub-Gateway/main.py
+++ iot project/RaspHub-Gateway/main.py
@@ -9,11 +9,11 @@ RADIO_FREQ_MHZ = 868.0
 CS = digitalio.DigitalInOut(board.CE1)
 RESET = digitalio.DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
-rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=1000000)
+LoRa = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+LoRa = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=1000000)
 
 while True:
-    sensor_data_message = rfm9x.receive()
+    sensor_data_message = LoRa.receive()
     if sensor_data_message is not None:
         sensor_data_string = str(received_data, "utf-8")
         publish.single("sg5-2a", sensor_data_string, hostname="mqtt.eclipseprojects.io")
